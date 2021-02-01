e0f26b3df14980af3f2ef666c5f27e405531e88e
My Lord <page.c.tyler@gmail.com>
2021-02-01 16:24:39 -0500

    refactored blue question one and added scala docs commenting.

---
+++ Question1.scala
@@ -0,0 +1,51 @@
+package blue
+
+import org.apache.spark.sql.functions.{max, sum}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object Question1 {
+  /**initialSolution
+   * takes in the dataframe that reflects the case and economic data output from DataFrameManipulator
+   * and outputs the data as formatted tables in the console as well as outputs the formatted data to dataframes in Spark.
+   * The DataFrameManipulator prepares raw data from two source files by selecting various, needed columns and renaming a few.
+   * The two tables are then joined with the joinCaseEcon method. The resulting dataframe is passed in this methods signature.
+   * The RankRegions class uses methods to group and sort by some metric passed as an argument. This is used just prior to displaying results.
+   *
+   * @param spark Spark Sessions instance for implicits.
+   * @param df  Fully prepared and joined dataset from the DataFrameManipulator class.
+   * @param resultPath Provides an results, output path for s3 storage.  However, currently not in use. This app only displays console results as of now.
+   */
+  def initialSolution(spark: SparkSession, df: DataFrame, resultPath: String): Unit ={
+    import spark.implicits._
+    val data = df
+      .withColumn("population",$"total_cases"/$"total_cases_per_million")
+      .groupBy("region", "country")
+      .agg(max($"population") as "population")
+      .groupBy("region")
+      .agg(sum($"population") as "population")
+
+    println("\nAverage New Cases per Day in Each Region")
+    RankRegions.rankByMetricLow(spark, data, "new_cases").show()
+
+    println("\nAverage New Cases per Million People per Day in Each Region (normalized before region grouping)")
+    RankRegions.rankByMetricLow(spark, data, "new_cases_per_million").show()
+
+    println("\nAverage New Cases per Million People per Day in Each Region")
+    RankRegions.rankByMetricLow(spark, data, "new_cases", "pop").show()
+
+    println("\nTotal Cases in Each Region")
+    RankRegions.rankByMetricLow(spark, data, "total_cases", "max").show()
+
+    println("\nTotal Cases per Million People in Each Region (normalized before region grouping)")
+    RankRegions.rankByMetricLow(spark, data, "total_cases_per_million", "max").show()
+
+    println("\nTotal Cases per Million People in Each Region")
+    RankRegions.rankByMetricLow(spark, data, "total_cases", "maxpop").show()
+
+    println("\nAverage GDP Percent Change in Each Region")
+    RankRegions.changeGDP(spark, data, "current_prices_gdp", percapita = false).show()
+
+    println("\nAverage GDP per Capita Percent Change in Each Region")
+    RankRegions.changeGDP(spark, data, "gdp_per_capita", percapita = false).show()
+  }
+}
